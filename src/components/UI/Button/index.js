eb27c248f237c5cff910ec30eb88afd62e1ed1de
Yuliia <julim1603@gmail.com>
2022-05-08 13:38:08 +0300

    fix button

--- src/components/UI/Button/index.js
+++ src/components/UI/Button/index.js
@@ -1,10 +1,7 @@
+import clsx from "clsx";
 import styles from "./style.module.css";
 
 const Button = (props) => {
-  return (
-    <button className={styles.button} onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+  return <button {...props} className={clsx(styles.button, props.className)} />;
 };
 export default Button;
